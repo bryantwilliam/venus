6d42e97dda06038adfd0cd6812f40a799cde2042
William Bryant <gogobebe2@gmail.com>
2022-05-02 05:08:14 +1200

    auth progress

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -12,20 +12,31 @@ class AuthGate extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
-          return Text("Show error message dialog"); // TODO
+          return Text("Show error message popup that can't be exited" +
+              snapshot.error.toString()); // TODO
         } else if (snapshot.connectionState == ConnectionState.waiting) {
           return Text(
               "splash or loading screen that says it's waiting to connect?"); // TODO
-        } else if (!snapshot.hasData /* || signup incomplete*/) {
-          // TODO also check if signup isn't completed
-          return const LoginPage();
-        } else {
+        } else if (snapshot.hasData) {
           User user = snapshot.data!;
 
+          // TODO remove debug prints
+          print("user 1: " + user.toString());
+          print("current user 1: " +
+              FirebaseAuth.instance.currentUser.toString());
           user.reload();
+          print("user 2: " + user.toString());
+          print("current user 2: " +
+              FirebaseAuth.instance.currentUser.toString());
 
-          print("AuthGate: user: " + user.toString());
+          // TODO
+          // if (if user deleted in firebase || signup incomplete*/) {
+          //     delete the user
+          //     return LoginPage();
+          // }
           return RootNavigationFluid(user);
+        } else {
+          return const LoginPage();
         }
       },
     );
