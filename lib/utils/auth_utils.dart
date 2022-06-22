4cb037407de9fcd5d0a55f0e25f05d888d13af72
William Bryant <gogobebe2@gmail.com>
2022-06-23 04:49:55 +1200

    auth

--- lib/utils/auth_utils.dart
+++ lib/utils/auth_utils.dart
@@ -1,6 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 bool isUserAccountSetup(User user) {
   return user.email != null && user.phoneNumber != null;
   // TODO check for user.displayName != null && gender, age or other mandatory details.
+}
+
+Future<void> logout() async {
+  await FacebookAuth.instance.logOut();
+  await FirebaseAuth.instance.signOut();
 }
