e07eb3dceb0d9d68ae2a5dcdb67bff94fb5890ae
William Bryant <gogobebe2@gmail.com>
2022-06-28 04:35:19 +1200

    login page background

--- lib/pages/sign_up/login_page.dart
+++ lib/pages/sign_up/login_page.dart
@@ -1,32 +1,79 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:venus/pages/sign_up/phone_signup_page.dart';
 import 'package:venus/utils/nav_utils.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 20),
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.linear,
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((animationStatus) {
+        if (animationStatus == AnimationStatus.completed) {
+          _animationController.repeat();
+        }
+      });
+    _animationController.forward();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          ElevatedButton(
-            onPressed: () {
-              pushNextPage(context, PhoneSignUpPage());
-            },
-            child: Text("Sign in with phone number"),
+          // TODO change this later to be a asset image.
+          CachedNetworkImage(
+            imageUrl:
+                "https://cdn.britannica.com/92/6792-004-C757C6D7/The-Birth-of-Venus-canvas-Sandro-Botticelli.jpg",
+            progressIndicatorBuilder: (context, url, downloadProgress) =>
+                CircularProgressIndicator(value: downloadProgress.progress),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+            alignment: FractionalOffset(_animation.value, 0),
           ),
-          ElevatedButton(
-            onPressed: () async {
-              await _signInWithFacebook();
-              // TODO check for error messages to display to user. If there are errors, just return; (facebook login failed, so do nothing)
-              pushNextPage(context, PhoneSignUpPage());
-            },
-            child: Text("Sign in with Facebook"),
+          Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  pushNextPage(context, PhoneSignUpPage());
+                },
+                child: Text("Sign in with phone number"),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await _signInWithFacebook();
+                  // TODO check for error messages to display to user. If there are errors, just return; (facebook login failed, so do nothing)
+                  pushNextPage(context, PhoneSignUpPage());
+                },
+                child: Text("Sign in with Facebook"),
+              ),
+            ],
           ),
         ],
       ),
@@ -46,5 +93,11 @@ class LoginPage extends StatelessWidget {
       return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
     }
     return null;
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
